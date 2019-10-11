8f9c856ccd1cfa87d4e0a2f2ba9f770423284a90
Guillaume Dery <dery.guillaume98@outlook.fr>
2019-10-11 15:29:01 +0200

    Adresse mail pro pour l'envoie de mail

--- htdocs/mail.php
+++ htdocs/mail.php
@@ -1,20 +1,20 @@
 <?php
- 
-    ini_set( 'display_errors', 1 );
- 
-    error_reporting( E_ALL );
- 
-    $from = "dery.guillaume98@outlook.fr";
- 
-    $to = $mail;
- 
-    $subject = "Confirmation Hackers Poulette";
- 
-    $message = "Votre requête a bien étée envoyée au service concerné";
- 
-    $headers = "From:" . $from;
- 
-    mail($to,$subject,$message, $headers);
- 
-    echo " Un email de confirmation à été envoyé à l'adresse ";
-    echo $mail;
+
+ini_set('display_errors', 1);
+
+error_reporting(E_ALL);
+
+$from = "no-reply@poulette.be";
+
+$to = $mail;
+
+$subject = "Confirmation Hackers Poulette";
+
+$message = "Votre requête a bien étée envoyée au service concerné";
+
+$headers = "From:" . $from;
+
+mail($to, $subject, $message, $headers);
+
+echo " Un email de confirmation à été envoyé à l'adresse ";
+echo $mail;
