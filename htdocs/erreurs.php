5dc86886877296431d3e8a63875f251eec004a48
Guillaume Dery <dery.guillaume98@outlook.fr>
2019-10-11 12:30:12 +0200

    Mail et vidage des variables quand validé

--- htdocs/erreurs.php
+++ htdocs/erreurs.php
@@ -1,8 +1,19 @@
 <?php
-if (!empty($_POST["nom"]) && !empty($_POST["prenom"]) && $_POST["sexe"] != "none" && !empty($_POST["mail"]) && $_POST["pays"] != "none" && !empty($_POST["message"])) {
+if (!empty($_POST["nom"]) && !empty($_POST["prenom"]) && $_POST["sexe"] != "none" && !empty($_POST["mail"]) && $_POST["pays"] != "none" && !empty($_POST["message"]) && empty($_POST["pot"])) {
     ?>
     <div class="col-12 valide">
         Formulaire envoyé !
+
+        <?php
+            include("mail.php");
+            //insérez requête SQL ici 
+            unset($_POST);
+            unset($_GET);
+            $nom = "";
+            $prenom = "";
+            $mail = "";
+            $message = "";
+            ?>
     </div>
 <?php
 } else {
